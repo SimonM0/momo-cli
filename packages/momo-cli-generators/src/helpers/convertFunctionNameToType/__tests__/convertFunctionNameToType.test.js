0bba52e05d09ce24a0713bba1878b15ffffdf4f2
Fried-Chicken <Fried-Chicken@users.noreply.github.com>
2018-10-20 19:30:45 +0200

    Fixed tests

--- packages/momo-cli-generators/src/helpers/convertFunctionNameToType/__tests__/convertFunctionNameToType.test.js
+++ packages/momo-cli-generators/src/helpers/convertFunctionNameToType/__tests__/convertFunctionNameToType.test.js
@@ -3,71 +3,72 @@ const convertFunctionNameToType = require('../convertFunctionNameToType');
 
 describe('convertFunctionNameToType', () => {
   it('should append Action to actions', () => {
-    const mockFunctionName = 'incrementCounter';
-    const expected = convertFunctionNameToType(
-      mockFunctionName,
-      FUNCTION_TYPES.ACTION,
-    );
+    const mockAnswers = {
+      functionName: 'incrementCounter',
+      functionType: FUNCTION_TYPES.ACTION,
+    };
+    const expected = convertFunctionNameToType(mockAnswers);
     const result = 'incrementCounterAction';
     expect(expected)
       .toEqual(result);
   });
   it('should append Container to containers and ', () => {
-    const mockFunctionName = 'headerBanner';
-    const expected = convertFunctionNameToType(
-      mockFunctionName,
-      FUNCTION_TYPES.CONTAINER,
-    );
+    const mockAnswers = {
+      functionName: 'headerBanner',
+      functionType: FUNCTION_TYPES.CONTAINER,
+    };
+    const expected = convertFunctionNameToType(mockAnswers);
     const result = 'HeaderBannerContainer';
     expect(expected)
       .toEqual(result);
   });
   it('should append Helper to helpers and ', () => {
-    const mockFunctionName = 'incrementCounter';
-    const expected = convertFunctionNameToType(
-      mockFunctionName,
-      FUNCTION_TYPES.HELPER,
-    );
+    const mockAnswers = {
+      functionName: 'incrementCounter',
+      functionType: FUNCTION_TYPES.HELPER,
+    };
+    const expected = convertFunctionNameToType(mockAnswers);
     const result = 'incrementCounterHelper';
     expect(expected)
       .toEqual(result);
   });
   it('should user function name for plain functions', () => {
-    const mockFunctionName = 'incrementCounter';
-    const expected = convertFunctionNameToType(
-      mockFunctionName,
-      FUNCTION_TYPES.PLAIN_FUNCTION,
-    );
+
+    const mockAnswers = {
+      functionName: 'incrementCounter',
+      functionType: FUNCTION_TYPES.PLAIN_FUNCTION,
+    };
+    const expected = convertFunctionNameToType(mockAnswers);
     const result = 'incrementCounter';
     expect(expected)
       .toEqual(result);
   });
   it('should append Reducer for Reducers', () => {
-    const mockFunctionName = 'incrementCounter';
-    const expected = convertFunctionNameToType(
-      mockFunctionName,
-      FUNCTION_TYPES.REDUCER,
-    );
+    const mockAnswers = {
+      functionName: 'incrementCounter',
+      functionType: FUNCTION_TYPES.REDUCER,
+    };
+    const expected = convertFunctionNameToType(mockAnswers);
     const result = 'incrementCounterReducer';
     expect(expected)
       .toEqual(result);
   });
   it('should append Selector for Selectors', () => {
-    const mockFunctionName = 'counterValue';
-    const expected = convertFunctionNameToType(
-      mockFunctionName,
-      FUNCTION_TYPES.SELECTOR,
-    );
+    const mockAnswers = {
+      functionName: 'counterValue',
+      functionType: FUNCTION_TYPES.SELECTOR,
+    };
+    const expected = convertFunctionNameToType(mockAnswers);
     const result = 'counterValueSelector';
     expect(expected)
       .toEqual(result);
   });
   it('should capitalize the first letter of components', () => {
-    const mockFunctionName = 'headerBanner';
-    const expected = convertFunctionNameToType(
-      mockFunctionName,
-      FUNCTION_TYPES.COMPONENT,
-    );
+    const mockAnswers = {
+      functionName: 'headerBanner',
+      functionType: FUNCTION_TYPES.COMPONENT,
+    };
+    const expected = convertFunctionNameToType(mockAnswers);
     const result = 'HeaderBanner';
     expect(expected)
       .toEqual(result);
